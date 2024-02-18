b16dc729a6ece7467721fb7e05b637d254c9ebcc
D.E <cameronqoo@gmail.com>
2024-02-19 03:11:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <AppHeader />
+  <div class="h-[3200px] bg-gray-100"></div>
+</template>
+
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import AppHeader from './components/layout/AppHeader.vue'
+</script>
