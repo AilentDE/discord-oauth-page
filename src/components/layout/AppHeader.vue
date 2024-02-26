e0d332eab412bf6c02f870a4c8d3320430b521b5
D.E <cameronqoo@gmail.com>
2024-02-26 14:17:03 +0800

    modify tailwind color config.

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,39 +1,40 @@
 <template>
   <header class="fixed w-full left-0 top-0 right-0 z-50 bg-white shadow-sm">
-    <div class="container mx-auto px-4 md:px-6 py-3">
+    <!-- main -->
+    <div class="container py-3">
       <div class="flex items-center justify-between w-full xl:w-auto">
         <!-- left -->
         <div class="flex items-center">
-          <button type="button" @click.prevent="toggle">
+          <button type="button" @click.prevent="toggleSubBar('sideBar')">
             <img class="mr-3 xl:hidden" src="@/assets/hamburger.svg" alt="Menu" />
           </button>
           <a href="https://clusters.tw/" class="flex items-center relative">
             <img class="w-24 xl:w-40" src="@/assets/logo.svg" alt="Logo" />
-            <div class="absolute -right-1 -top-2 w-4 h-4">
+            <div class="absolute -right-1 -top-2 size-4">
               <IconDiscord class="fill-[#5562EA]" />
             </div>
           </a>
           <ul class="ml-6 items-center hidden xl:flex">
-            <li class="px-3 py-2 text-gray-600 hover:text-black">
+            <li class="px-3 py-2 text-gray hover:text-black">
               <a href="https://clusters.tw/">首頁</a>
             </li>
-            <li class="px-3 py-2 text-gray-600 hover:text-black">
+            <li class="px-3 py-2 text-gray hover:text-black">
               <a href="https://clusters.tw/explore">全站探索</a>
             </li>
           </ul>
         </div>
         <!-- right mobile -->
         <div class="flex items-center xl:hidden space-x-4">
-          <button type="button" @click.prevent="toggleSubBar('search')">
-            <IconSearch class="w-5 h-5 mr-2 fill-[#BDBDBD] hover:fill-gray-500" />
+          <button type="button" @click.prevent="toggleSubBar('searchBar')">
+            <IconSearch class="size-5 mr-2 fill-[#BDBDBD] hover:fill-gray-500" />
           </button>
-          <button type="button">
-            <IconAlarm class="w-8 h-8 fill-[#BDBDBD] hover:fill-gray-500" />
+          <button type="button" @click.prevent="toggleSubBar('notificationBar')">
+            <IconAlarm class="size-8 fill-[#BDBDBD] hover:fill-gray-500" />
           </button>
         </div>
         <!-- right PC -->
         <div class="items-center hidden xl:flex">
-          <div class="bg-gray-100 flex mr-5 px-3 py-2 w-[9.5rem]">
+          <div class="bg-primary-lighter flex mr-5 px-3 py-2 w-[9.5rem]">
             <button @click="search">
               <IconSearch class="mr-2 fill-[#BDBDBD] hover:fill-gray-500" />
             </button>
@@ -46,26 +47,23 @@
             />
           </div>
           <button type="button" class="mr-3">
-            <IconAlarm class="w-8 h-8 fill-[#BDBDBD] hover:fill-gray-500" />
+            <IconAlarm class="size-8 fill-[#BDBDBD] hover:fill-gray-500" />
           </button>
           <button
             type="button"
-            class="bg-[#3892d3] border-[#3892d3] mb-1 px-4 py-2 text-sm text-white justify-center items-center rounded-full border hover:bg-[#013c85] hover:border-[#013c85] duration-300"
+            class="bg-primary border-primary mb-1 px-4 py-2 text-sm text-white justify-center items-center rounded-full border hover:bg-primary-dark hover:border-primary-dark duration-300"
             @click="publish"
           >
             發布
           </button>
-          <button class="ml-5 mb-1">
-            <div class="h-10 w-10 rounded-full bg-gray-600"></div>
+          <button class="animate-pulse ml-5 mb-1">
+            <div class="size-10 rounded-full bg-gray-600"></div>
           </button>
         </div>
       </div>
     </div>
     <!-- mobile search bar -->
-    <div
-      class="container mx-auto px-4 md:px-6 pb-2 -pt-1 xl:hidden"
-      v-show="showSubBar === 'search'"
-    >
+    <div class="container pb-2 -pt-1 xl:hidden" v-show="showSubBar === 'searchBar'">
       <div class="flex px-3 py-2 bg-gray-100 rounded">
         <button @click="search">
           <IconSearch class="mr-2 fill-[#BDBDBD] hover:fill-gray-500" />
@@ -80,6 +78,59 @@
       </div>
     </div>
   </header>
+  <!-- mobile side bar -->
+  <div
+    class="fixed top-12 bottom-0 left-0 z-30 bg-white shadow-sm w-[13.5rem] py-4 xl:hidden"
+    v-show="showSubBar === 'sideBar'"
+  >
+    <div class="animate-pulse flex items-center space-x-1 px-3 py-2">
+      <div class="size-10 rounded-full bg-gray-600"></div>
+      <div class="font-bold">User Name</div>
+    </div>
+    <div class="py-2 px-4">
+      <button
+        type="button"
+        class="bg-primary border-primary mb-1 px-4 py-2 text-sm text-white justify-center items-center rounded-full w-full border hover:bg-primary-dark hover:border-primary-dark duration-300"
+        @click="publish"
+      >
+        發布
+      </button>
+      <div class="divide-y devide-gray-lighter">
+        <ul class="text-gray text-sm">
+          <li class="py-3 hover:text-black">首頁</li>
+          <li class="py-3 hover:text-black">全站探索</li>
+          <li class="py-3 hover:text-black">我的創作者頁面</li>
+        </ul>
+        <ul class="text-gray text-sm">
+          <li class="py-3 hover:text-black">個人檔案</li>
+          <li class="py-3 hover:text-black">訂閱管理</li>
+          <li class="py-3 hover:text-black">企劃管理</li>
+          <li class="py-3 hover:text-black">訂單管理</li>
+          <li class="py-3 hover:text-black">金流管理</li>
+        </ul>
+        <ul class="text-gray text-sm">
+          <li class="py-3 hover:text-black">返回</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- mobile notification bar -->
+  <div
+    class="fixed top-12 bottom-0 right-0 z-30 bg-white shadow-sm w-60 py-4 text-sm xl:hidden"
+    v-show="showSubBar === 'notificationBar'"
+  >
+    <div class="container">
+      <div class="flex justify-between">
+        <div class="py-2 font-medium text-left">通知停用中</div>
+      </div>
+    </div>
+  </div>
+  <!-- bar background -->
+  <div
+    class="fixed inset-0 z-20 bg-black bg-opacity-50 xl:hidden"
+    v-show="!!showSubBar"
+    @click.prevent="toggleSubBar(null)"
+  ></div>
 </template>
 
 <script setup>
@@ -87,12 +138,6 @@ import { ref } from 'vue'
 import IconDiscord from '@/components/icons/IconDiscord.vue'
 import IconSearch from '@/components/icons/IconSearch.vue'
 import IconAlarm from '@/components/icons/IconAlarm.vue'
-
-const open = ref(false)
-const toggle = () => {
-  open.value = !open.value
-  console.log(open.value)
-}
 
 const showSubBar = ref(null)
 const toggleSubBar = (val) => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem'
+      }
+    },
+    extend: {
+      colors: {
+        gray: {
+          lightest: '#f5f5f5',
+          lighter: '#e0e0e0',
+          light: '#bdbdbd',
+          DEFAULT: '#757575',
+          dark: '#424242',
+          darker: '#212121'
+        },
+        primary: {
+          lighter: '#f1f3f7',
+          light: '#e6f0f8',
+          DEFAULT: '#3892d3',
+          dark: '#013c85',
+          darker: '#00132b'
+        },
+        secondary: {
+          light: '#9fa8da',
+          DEFAULT: '#3f51b5',
+          dark: '#283593'
+        }
+      }
+    }
   },
   plugins: []
 }
